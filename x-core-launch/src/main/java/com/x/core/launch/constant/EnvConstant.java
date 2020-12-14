e2c5461b62529d5faeb6050563ed8a74d77b280d
weihuijie@9186.com <weihuijie521>
2020-12-14 18:42:00 +0800

    项目初始化。 解决： 1、子模块继承父级模块的依赖 2、自定义项目启动器

---
+++ x-core-launch/src/main/java/com/x/core/launch/constant/EnvConstant.java
@@ -0,0 +1,25 @@
+package com.x.core.launch.constant;
+
+/**
+ *  环境常量
+ */
+public class EnvConstant {
+
+    /**
+     * 开发环境
+     */
+    public static final String DEV_CODE = "dev";
+    /**
+     * 生产环境
+     */
+    public static final String PROD_CODE = "prod";
+    /**
+     * 测试环境
+     */
+    public static final String TEST_CODE = "test";
+
+    /**
+     * 代码部署于 linux 上，工作默认为 mac 和 Windows
+     */
+    public static final String OS_NAME_LINUX = "LINUX";
+}
